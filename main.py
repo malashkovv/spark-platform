4dba353083b4be3bd4ad3f1f0b5ebc9ed072a993
Valentin Malashkov <malashkovv@gmail.com>
2021-09-26 21:46:55 +0300

    Added more versions

--- main.py
+++
@@ -1,9 +0,0 @@
-from pyspark import SparkConf, SparkContext
-from src.handler import handle
-from src.log import logger
-
-conf = SparkConf().setMaster("spark://master:7077").setAppName("Test")
-
-with SparkContext(conf=conf) as sc:
-    logger.info("Invoking handler.")
-    handle(sc)
