965eaa22d8657ffd90566383f38bb21063777054
Valentin Malashkov <malashkovv@gmail.com>
2022-01-05 22:31:30 +0300

    Added additional arguments

--- entrypoint.sh
+++ entrypoint.sh
@@ -29,26 +29,29 @@ set_jupyter() {
 set_master_uri() {
   if [ ${MODE} == 'LOCAL' ]; then
     export MASTER_URI="local[*]"
-  else
+  elif [ ${MODE} == 'STANDALONE' ]; then
     export MASTER_URI="spark://${SPARK_MASTER_HOST}:${SPARK_MASTER_PORT}"
+  else
+    echo "Wrong MODE=${MODE}. Aborting."
+    exit 1
   fi
 }
 
 run() {
   set_master_uri
-  exec $SPARK_HOME/bin/$1 --master $MASTER_URI
+  exec $SPARK_HOME/bin/$1 --master $MASTER_URI ${@:2}
 }
 
 if [ $1 == 'spark-ipython' ]; then
     set_ipython
-    run pyspark
+    run pyspark ${@:2}
 elif [ $1 == 'spark-jupyter' ]; then
     set_jupyter
-    run pyspark
+    run pyspark ${@:2}
 elif [ $1 == 'spark-sql' ]; then
-    run spark-sql
-elif [ $1 == 'submit' ]; then
-    exec $SPARK_HOME/bin/spark-submit $2
+    run spark-sql ${@:2}
+elif [ $1 == 'spark-submit' ]; then
+    run spark-submit ${@:2}
 elif [ $1 == 'worker' ]; then
     exec $SPARK_HOME/bin/spark-class org.apache.spark.deploy.worker.Worker spark://${SPARK_MASTER_HOST}:${SPARK_MASTER_PORT}
 elif [ $1 == 'master' ]; then
